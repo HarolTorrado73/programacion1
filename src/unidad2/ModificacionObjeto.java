8d49b35ab94e35dcc406541d8c4af00ce8da0ede
HarolTorrado73 <ubuntohcmelot@gmail.com>
2025-05-27 06:25:45 -0500

    ultimos ejercicios U2

---
+++ src/unidad2/ModificacionObjeto.java
@@ -0,0 +1,15 @@
+public class ModificacionObjeto {
+
+    public static void modificarRectangulo(Rectangulo r) {
+        r.ancho = 50;
+        r.alto = 30;
+        System.out.println("Dentro del método: ancho = " + r.ancho + ", alto = " + r.alto);
+    }
+
+    public static void main(String[] args) {
+        Rectangulo miRectangulo = new Rectangulo(10, 20);
+        System.out.println("Antes de modificar: ancho = " + miRectangulo.ancho + ", alto = " + miRectangulo.alto);
+        modificarRectangulo(miRectangulo);
+        System.out.println("Después de modificar: ancho = " + miRectangulo.ancho + ", alto = " + miRectangulo.alto);
+    }
+} 
---
+++ src/unidad2/Rectangulo.java
@@ -0,0 +1,9 @@
+class Rectangulo {
+    int ancho;
+    int alto;
+
+    Rectangulo(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+} 
